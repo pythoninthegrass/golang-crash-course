d05e0967ba8c980399aefd85a83e801b6283341d
Nana Janashia <nn_janashia@yahoo.com>
2024-10-07 07:22:06 +0000

    Update 4-loops.go

--- 4-loops.go
+++ 4-loops.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var shortGolang = "Watch Go crash course"
-	var fullGolang = "Watch Nana's Golang Full Course"
-	var rewardDessert = "Reward myself with a donut"
+  var shortGolang = "Watch Go crash course"
+  var fullGolang = "Watch Nana's Golang Full Course"
+  var rewardDessert = "Reward myself with a donut"
 
-	var taskItems = []string{shortGolang, fullGolang, rewardDessert}
+  var taskItems = []string{shortGolang, fullGolang, rewardDessert}
 
-	fmt.Println("###### Welcome to our Todolist App! ######")
-	fmt.Println("List of my Todos")
+  fmt.Println("###### Welcome to our Todolist App! ######")
+  fmt.Println("List of my Todos")
 
-	for index, task := range taskItems {
-		fmt.Printf("%d: %s\n", index+1, task)
-	}
+  for index, task := range taskItems {
+	  fmt.Printf("%d: %s\n", index+1, task)
+  }
 }
