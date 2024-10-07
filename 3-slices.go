dc4af97f00aad7cec3a8977874340da6079a38eb
Nana Janashia <nn_janashia@yahoo.com>
2024-10-07 07:22:58 +0000

    Update 3-slices.go

--- 3-slices.go
+++ 3-slices.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var shortGolang = "Watch Go crash course"
-	var fullGolang = "Watch Nana's Golang Full Course"
-	var rewardDessert = "Reward myself with a donut"
+  var shortGolang = "Watch Go crash course"
+  var fullGolang = "Watch Nana's Golang Full Course"
+  var rewardDessert = "Reward myself with a donut"
 
-	var taskItems = []string {shortGolang, fullGolang, rewardDessert}
+  var taskItems = []string {shortGolang, fullGolang, rewardDessert}
+
+  fmt.Println("###### Welcome to our Todolist App! ######")
 	
-	fmt.Println("###### Welcome to our Todolist App! ######")
-    	
-	fmt.Println("List of my Todos")
-	fmt.Println("Tasks:", taskItems)
+  fmt.Println("List of my Todos")
+  fmt.Println("Tasks:", taskItems)
 }
