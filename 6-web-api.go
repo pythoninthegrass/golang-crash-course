21244ef75ccc3103ba5cd067e3f067405b959895
Nana Janashia <nn_janashia@yahoo.com>
2024-10-07 07:20:20 +0000

    Update 6-web-api.go

--- 6-web-api.go
+++ 6-web-api.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
 var shortGolang = "Watch Go crash course"
@@ -11,18 +11,18 @@ var rewardDessert = "Reward myself with a donut"
 var taskItems = []string{shortGolang, fullGolang, rewardDessert}
 
 func main() {
-	http.HandleFunc("/", helloUser)
-	http.HandleFunc("/show-tasks", showTasks)
-	http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", helloUser)
+  http.HandleFunc("/show-tasks", showTasks)
+  http.ListenAndServe(":8080", nil)
 }
 
 func showTasks(writer http.ResponseWriter, request *http.Request) {
-	for _, task := range taskItems {
-		fmt.Fprintln(writer, task)
-	}
+  for _, task := range taskItems {
+    fmt.Fprintln(writer, task)
+  }
 }
 
 func helloUser(writer http.ResponseWriter, request *http.Request) {
-	var greeting = "Hello user. Welcome to our Todolist App!"
-	fmt.Fprintln(writer, greeting)
+  var greeting = "Hello user. Welcome to our Todolist App!"
+  fmt.Fprintln(writer, greeting)
 }
